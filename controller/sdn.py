23d7afbdbf7d79e60bbd0322bb04704f443dd551
Tomás Gaspar <tomasc.gaspar@hotmail.com>
2025-05-05 21:33:52 +0100

    controller and network setup

---
+++ controller/sdn.py
@@ -0,0 +1,57 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib import hub
+
+class OnionController(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(OnionController, self).__init__(*args, **kwargs)
+        self.datapaths = {}
+        self.monitor_thread = hub.spawn(self._monitor)
+        self.logger.info("SDN Controller initialized.")
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        self.logger.info(f"Switch connected: {datapath.id}")
+        self.datapaths[datapath.id] = datapath
+
+        # Add a default flow to send unmatched packets to controller
+        parser = datapath.ofproto_parser
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(datapath.ofproto.OFPP_CONTROLLER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=priority,
+            match=match,
+            instructions=inst
+        )
+        datapath.send_msg(mod)
+
+    def _monitor(self):
+        while True:
+            for dp in self.datapaths.values():
+                self._request_stats(dp)
+            hub.sleep(5)
+
+    def _request_stats(self, datapath):
+        self.logger.info(f"Requesting stats from switch {datapath.id}")
+        parser = datapath.ofproto_parser
+        req = parser.OFPFlowStatsRequest(datapath)
+        datapath.send_msg(req)
+
+    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    def _flow_stats_reply_handler(self, ev):
+        body = ev.msg.body
+        self.logger.info("Flow statistics received:")
+        for stat in body:
+            self.logger.info(f"Match: {stat.match}, Packets: {stat.packet_count}, Bytes: {stat.byte_count}")
