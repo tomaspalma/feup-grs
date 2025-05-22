8bc13ea2ac56138f73b869759024737aab102018
Tomás Gaspar <tomasc.gaspar@hotmail.com>
2025-05-22 21:51:13 +0100

    fixed onion dockerfile problem

--- onion/entrypoint.sh
+++ onion/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Get the hostname and extract the task slot
 REPLICA_ID=$(hostname | cut -d'.' -f2)
 export REPLICA_NAME="onion-$REPLICA_ID"
 
@@ -11,6 +10,4 @@ echo "REPLICA_NAME=$REPLICA_NAME" >> .env
 ADDRESS=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 echo "ADDRESS=$ADDRESS" >> .env
 
-python3 onion.py
-
-sleep infinity
+python3 onion.py & /usr/local/bin/node_exporter
